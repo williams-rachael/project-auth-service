ebe907ca26bbb080bc732503d6546d99c648c0e5
Rachael <williams_rachael@bah.com>
2021-08-04 13:18:14 -0400

    Commenting tests and test dependencies

--- project-auth-service/src/test/java/com/bah/msd/ProjectAuthServiceApplicationTests.java
+++ project-auth-service/src/test/java/com/bah/msd/ProjectAuthServiceApplicationTests.java
@@ -1,13 +1,13 @@
-package com.bah.msd;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ProjectAuthServiceApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.bah.msd;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ProjectAuthServiceApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
